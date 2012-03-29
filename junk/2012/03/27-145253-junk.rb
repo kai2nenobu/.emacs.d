87687c4d6f67e7daa4b255fc403ead4c3c7d45cc
Tsunenobu Kai <kai2nenobu@gmail.com>
2012-03-30 05:00:40 +0900

    ひととおり ruby の環境を整えたので，ジャンクファイルで色々実験．

---
+++ junk/2012/03/27-145253-junk.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+# RSense がちゃんと動くか実験．
+# おおむねうごく．るりまからリファレンスすることもできる．
+# しかし，いまいち auto-complete-rsense が発動する条件がわからない．
+# auto-complete-rsense であれば Object m とかの補助文字が出るはずだが，
+# でないときもあるのがよくわからない．
+
+include Math.
+
+=begin
+ghaudiga
+hoge
+=end
+
+
+
+
+
+if condition
+  
+end
---
+++ junk/2012/03/28-130805-junk.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+10.times do |i|
+  i                             # 
+end
+
+
+a = [1,2,3,4,5]
+a.map{ |i| i*2}                 # =>
+
+puts "ほげ"
+puts a
