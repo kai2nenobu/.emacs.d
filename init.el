0eaa5604db276f4ca703f416822b8465dbe7e824
Tsunenobu Kai <kai2nenobu@gmail.com>
2012-06-21 19:03:54 +0900

    エクスポート時に，インデントは無視したほうが都合がいいような気がしてきたのでそのように設定．
    なぜこれを t にしていたかわからない．

--- init.el
+++ init.el
@@ -6,5 +6,5 @@
 (add-to-list 'load-path (expand-file-name "site-lisp/org-mode" user-emacs-directory))
 (defvar org-init-directory (expand-file-name "org-init.d" user-emacs-directory))
 (require 'org-install)
-(setq org-src-preserve-indentation t)   ; エクスポートでインデントを保持する
+;(setq org-src-preserve-indentation t)   ; エクスポートでインデントを保持する
 (org-babel-load-file (expand-file-name "init.org" org-init-directory))
