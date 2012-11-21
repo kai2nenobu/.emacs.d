1ee0bda00c0fd0ceca244a89ed67b8040c99bf7b
Tsunenobu Kai <kai2nenobu@gmail.com>
2012-11-21 16:57:35 +0900

    org-install は obsolete なので，かわりに (require 'org) に変更．

--- init.el
+++ init.el
@@ -5,6 +5,6 @@
 ;;; org-babel を使って初期化ファイルをロード
 (add-to-list 'load-path (expand-file-name "site-lisp/org-mode" user-emacs-directory))
 (defvar org-init-directory (expand-file-name "org-init.d" user-emacs-directory))
-(require 'org-install)
+(require 'org)
 ;(setq org-src-preserve-indentation t)   ; エクスポートでインデントを保持する
 (org-babel-load-file (expand-file-name "init.org" org-init-directory))
