ed40e28af5179a0462014da75aca2e4f3e96be5e
Tsunenobu Kai <kai2nenobu@gmail.com>
2016-11-21 11:29:41 +0900

    カスタム設定が自動で追記された

--- init-custom.el
+++ init-custom.el
@@ -1,1 +1,33 @@
-()
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dash flycheck git-commit go-mode helm helm-core org with-editor yasnippet zlc zeal-at-point yaml-mode wrap-region which-key web-mode use-package typescript-mode stripe-buffer smex smartrep smartparens smart-newline session recentf-ext rainbow-mode quickrun projectile prodigy powershell plantuml-mode pallet ox-reveal ox-pandoc org-plus-contrib org-octopress open-junk-file nyan-mode mykie multiple-cursors moz-controller migemo markdown-mode magit mag-menu lispxmp kotlin-mode key-chord js2-mode irfc idle-highlight-mode httprepl hiwin highlight-symbol helm-swoop helm-package helm-ls-git helm-git-grep helm-descbinds helm-c-yasnippet guide-key groovy-mode graphviz-dot-mode gradle-mode go-eldoc go-autocomplete git-gutter-fringe flycheck-cask feature-mode expand-region exec-path-from-shell evil emmet-mode elscreen editorconfig ecukes drag-stuff direx dired+ csv-mode color-moccur bm auto-save-buffers-enhanced anzu adoc-mode ace-jump-mode)))
+ '(safe-local-variable-values
+   (quote
+    ((quickrun-option-timeout-seconds . 10)
+     (quickrun-option-args . "")
+     (quickrun-option-cmdopt . ""))))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cfw:face-day-title ((t :background "grey10")))
+ '(cfw:face-default-content ((t :foreground "green2")))
+ '(cfw:face-header ((t (:foreground "maroon2" :weight bold))))
+ '(cfw:face-holiday ((t :background "grey10" :foreground "purple" :weight bold)))
+ '(cfw:face-regions ((t :foreground "cyan")))
+ '(cfw:face-saturday ((t :foreground "blue" :weight bold)))
+ '(cfw:face-select ((t :background "blue4")))
+ '(cfw:face-sunday ((t :foreground "red" :weight bold)))
+ '(cfw:face-title ((t (:foreground "darkgoldenrod3" :weight bold :height 2.0 :inherit variable-pitch))))
+ '(cfw:face-today ((t :foreground: "cyan" :weight bold)))
+ '(cfw:face-today-title ((t :background "red4" :weight bold)))
+ '(col-highlight ((t (:background "gray10"))))
+ '(scroll-bar ((t :foreground "magenta")))
+ '(twittering-uri-face ((t (:foreground "cyan" :underline t)))))
