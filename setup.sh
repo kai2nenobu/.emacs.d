9be3da269f84e3abc93ade1dcd5342474f521cdc
Tsunenobu Kai <kai2nenobu@gmail.com>
2017-09-26 09:29:57 +0900

    変数を小文字に変更する

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 ## フックスクリプトにシンボリックリンクをはる
-HOOK_DIR=$DIR/hooks
-TARGET_DIR=$DIR/.git/hooks
+hook_dir=$DIR/hooks
+target_dir=$DIR/.git/hooks
 (
-  cd "$HOOK_DIR"
+  cd "$hook_dir"
   for hook in *; do
-    ln -s -f -t "$TARGET_DIR" "../../hooks/$hook"
+    ln -s -f -t "$target_dir" "../../hooks/$hook"
   done
 )
