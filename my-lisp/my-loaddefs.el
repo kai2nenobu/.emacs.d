8133b4e35ddbd6a90a25b9d8299807efc21354c2
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-03-20 04:33:08 +0900

    自作elispのautoloadを抽出する

---
+++ my-lisp/my-loaddefs.el
@@ -0,0 +1,79 @@
+;;; my-loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "bom-mode" "bom-mode.el" (23194 63680 0 0))
+;;; Generated autoloads from bom-mode.el
+
+(autoload 'bom-mode "bom-mode" "\
+Visualize BOM in buffer.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "java-options" "java-options.el" (22650 4464
+;;;;;;  0 0))
+;;; Generated autoloads from java-options.el
+
+(autoload 'java-options "java-options" "\
+Java option menu.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "qv-mode" "qv-mode.el" (22650 4464 0 0))
+;;; Generated autoloads from qv-mode.el
+
+(autoload 'qv-mode "qv-mode" "\
+Major mode for QlikView Load Script
+
+\\{qv-mode-map}
+
+\(fn)" t nil)
+
+(add-to-list 'magic-mode-alist (cons "///\\$tab Main" 'qv-mode))
+
+;;;***
+
+;;;### (autoloads nil "shell-session-mode" "shell-session-mode.el"
+;;;;;;  (23195 9291 0 0))
+;;; Generated autoloads from shell-session-mode.el
+
+(autoload 'shell-session-mode "shell-session-mode" "\
+Major mode for shell session
+
+\\{shell-session-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "uiflow-mode" "uiflow-mode.el" (22898 753 0
+;;;;;;  0))
+;;; Generated autoloads from uiflow-mode.el
+
+(autoload 'uiflow-mode "uiflow-mode" "\
+Major mode for UI Flow script
+
+\\{uiflow-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("emoji-cheat-sheet++.el" "loaddefs.el")
+;;;;;;  (23216 3852 0 0))
+
+;;;***
+
+(provide 'my-loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; my-loaddefs.el ends here
