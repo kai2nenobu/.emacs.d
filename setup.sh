85466429bbd6beb27c9388e53881546e9b435c6e
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-02-06 11:02:06 +0900

    シンボリックリンクを張る変わりに、core.hooksPathを設定する
    git 2.9から core.hooksPath という設定が追加され、フックスクリプトの
    配置位置を変更できるようになった。いままではわざわざ .git/hooks にシンボリックリンクを
    張っていたが、設定を変えるだけで良くなった。楽ちん。
    
    - https://qiita.com/kawaz/items/cf8d2a6675e5e3e66e50
    
    あと、シンボリックリンクがうまく動こうように core.symlinks を true に設定してる。
    ただクローンするときに true になってないと、チェックアウト時にシンボリックリンクにならないかも。
    また試してみる。

--- setup.sh
+++ setup.sh
@@ -6,38 +6,11 @@
 ### 【実行方法】
 ###   $ ./setup.sh
 ###
-### 【注意】
-### Windowsの場合、シンボリックリンクをはるのに管理者権限が必要になる。
-###
 
 DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-force=false
-while getopts :f OPT; do
-  case $OPT in
-    f|+f)
-      force=true
-      ;;
-    *)
-      echo "usage: `basename $0` [+-f} [--] ARGS..."
-      exit 2
-  esac
-done
-shift `expr $OPTIND - 1`
-OPTIND=1
+# フックスクリプトの場所を変更する
+git config core.hooksPath 'hooks'
 
-## フックスクリプトにシンボリックリンクをはる
-hook_dir=$DIR/hooks
-target_dir=$DIR/.git/hooks
-(
-  cd "$hook_dir"
-  for hook in *; do
-    target_hook=${target_dir}/${hook}
-    if [ $force = "false" ] && [ -e "$target_hook" ]; then
-      echo "${target_hook} already exists. If you want to overwite, Add \"-f\" option." 2>&1
-      exit 1
-    fi
-    rm -rf "$target_hook"
-    ln -s -f -t "$target_dir" "../../hooks/$hook"
-  done
-)
+# シンボリックリンクを有効にする
+git config core.symlinks true
