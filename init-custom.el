63753cb43a1644da410f6d109ff19ff04c389847
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-09-03 12:47:10 +0900

    現在のcustom設定を書き出す

--- init-custom.el
+++ init-custom.el
@@ -5,29 +5,15 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (emoji-cheat-sheet-plus dockerfile-mode htmlize magit-popup org-mac-link sql-indent dash flycheck git-commit go-mode helm helm-core org with-editor yasnippet zlc zeal-at-point yaml-mode wrap-region which-key web-mode use-package typescript-mode stripe-buffer smex smartrep smartparens smart-newline session recentf-ext rainbow-mode quickrun projectile prodigy powershell plantuml-mode pallet ox-reveal ox-pandoc org-plus-contrib org-octopress open-junk-file nyan-mode mykie multiple-cursors moz-controller migemo markdown-mode magit mag-menu lispxmp kotlin-mode key-chord js2-mode irfc idle-highlight-mode httprepl hiwin highlight-symbol helm-swoop helm-package helm-ls-git helm-git-grep helm-descbinds helm-c-yasnippet guide-key groovy-mode graphviz-dot-mode gradle-mode go-eldoc go-autocomplete git-gutter-fringe flycheck-cask feature-mode expand-region exec-path-from-shell evil emmet-mode elscreen editorconfig ecukes drag-stuff direx dired+ csv-mode color-moccur bm auto-save-buffers-enhanced anzu adoc-mode ace-jump-mode)))
- '(safe-local-variable-values
-   (quote
-    ((quickrun-option-timeout-seconds . 10)
-     (quickrun-option-args . "")
-     (quickrun-option-cmdopt . ""))))
+    (mozc-posframe zoom-window zlc zeal-at-point wrap-region which-key web-mode volatile-highlights undo-tree typescript-mode tss toml-mode sudo-ext stripe-buffer sql-indent smartrep smart-newline session save-load-path recentf-ext rainbow-mode quickrun quelpa-use-package powershell popwin plantuml-mode php-mode ox-reveal ox-pandoc ox-hugo outline-magic org-tree-slide org-re-reveal org-plus-contrib org-octopress open-junk-file mykie multiple-cursors mozc-im moz-controller migemo markdown-mode magit-delta lispxmp kotlin-mode key-chord js2-mode hiwin highlight-symbol highlight-indent-guides helm-swoop helm-orgcard helm-ls-git helm-git-grep helm-ghq helm-descbinds helm-c-yasnippet helm-bibtex helm-ag groovy-mode graphviz-dot-mode gradle-mode google-maps google go-eldoc go-autocomplete git-gutter-fringe flycheck-inline fill-column-indicator expand-region exec-path-from-shell esup emojify emoji-cheat-sheet-plus emmet-mode elscreen editorconfig dockerfile-mode docker-tramp docker-compose-mode direx dired+ delight csv-mode concurrent color-moccur c-eldoc beacon auto-save-buffers-enhanced auto-async-byte-compile atomic-chrome anzu ahk-mode adoc-mode ace-jump-mode)))
+ '(quelpa-checkout-melpa-p nil nil nil "MELPAリポジトリをクローンしない")
+ '(quelpa-melpa-recipe-stores nil nil nil "MELPAレシピの配置場所")
+ '(quelpa-update-melpa-p nil nil nil "MELPAリポジトリをアップデートしない")
+ '(quelpa-upgrade-p nil nil nil "パッケージを自動アップデートしない")
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cfw:face-day-title ((t :background "grey10")))
- '(cfw:face-default-content ((t :foreground "green2")))
- '(cfw:face-header ((t (:foreground "maroon2" :weight bold))))
- '(cfw:face-holiday ((t :background "grey10" :foreground "purple" :weight bold)))
- '(cfw:face-regions ((t :foreground "cyan")))
- '(cfw:face-saturday ((t :foreground "blue" :weight bold)))
- '(cfw:face-select ((t :background "blue4")))
- '(cfw:face-sunday ((t :foreground "red" :weight bold)))
- '(cfw:face-title ((t (:foreground "darkgoldenrod3" :weight bold :height 2.0 :inherit variable-pitch))))
- '(cfw:face-today ((t :foreground: "cyan" :weight bold)))
- '(cfw:face-today-title ((t :background "red4" :weight bold)))
- '(col-highlight ((t (:background "gray10"))))
- '(scroll-bar ((t :foreground "magenta")))
- '(twittering-uri-face ((t (:foreground "cyan" :underline t)))))
+ )
